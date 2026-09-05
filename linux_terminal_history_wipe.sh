673ecad7316d8437ab72649e05a407b6ca1be28f
s2119p <sudhirpokharel@gmail.com>
2026-09-05 08:57:31 +0545

    Update linux_terminal_history_wipe.sh

--- linux_terminal_history_wipe.sh
+++ linux_terminal_history_wipe.sh
@@ -1,5 +1,5 @@
-rm -f ~/.ash_history ~/.bash_history ~/.zsh_history 2>/dev/null
-export HISTFILE=/dev/null
 set +o history 2>/dev/null || true
+export HISTFILE=/dev/null
+rm -f ~/.ash_history ~/.bash_history ~/.zsh_history ~/.history 2>/dev/null || true
 history -c 2>/dev/null || true
 printf "\033[0;32m[✓] History wiped and recording disabled.\033[0m\n"
